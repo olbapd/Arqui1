37b79cb4e1a96fe6d4fd53a511e4290fbf6bd47c
JoseRIvera07 <ce.joserivera7@gmail.com>
2019-08-25 17:33:33 -0600

    Fetch updated

--- Proyecto1/CPU-Steps/Fetch.sv
+++ Proyecto1/CPU-Steps/Fetch.sv
@@ -1,20 +1,20 @@
 module Fetch (
-    input logic CLK, RST, 
-    input fetch_decode_t fetch_decode_i,
-    output fetch_decode_t fetch_decode_o
-);
+		input logic CLK, RST, PCSrc,
+		input logic [31:0] WD3,
+		output logic [31:0] R15, PC, Instr
+        );
 
 logic [31:0] PCPlus4;
 logic [31:0] PCP;
 
-n_bit_register #(32) pc_reg(CLK, RST, PCP, 1'b1, fetch_decode_o.PC);
+n_bit_register #(32) pc_reg(CLK, RST, PCP, 1'b1, PC);;   //PC register
 
-assign PCPlus4 = fetch_decode_o.PC+ 4;
+assign PCPlus4 = PC+ 4;                                  //PCPLUS4
 
-n_bit_mux #(32, 1) pc_mux ({fetch_decode_i.WD3, PCPlus4}, fetch_decode_i.PCSrc, PCP);
+n_bit_mux #(32, 1) pc_mux ({WD3, PCPlus4}, PCSrc, PCP);  // PC MUX
 
-assign fetch_decode_o.R15 = PCPlus4 + 4;
+assign fetch_decode_o.R15 = PCPlus4 + 4;                 // PCPlus8
 
-imem imem(fetch_decode_o.PC, fetch_decode_o.Instr);
+imem imem(PC, Instr);                                    // Intruction Memory
 
 endmodule
