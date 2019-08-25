965ca2835877a2e9d5a6a07d8c25744bc7f9ffe2
JoseRIvera07 <ce.joserivera7@gmail.com>
2019-08-25 14:45:01 -0600

    Fetch step added

--- Proyecto1/CPU-Steps/Fetch.sv
+++ Proyecto1/CPU-Steps/Fetch.sv
@@ -1,5 +1,19 @@
 module Fetch (
-    input logic clk, rst, pcsrc,
-    input logic [31:0] wd3,
-    output logic [31:0] r15,pc
+    input logic CLK, RST, 
+    input fetch_decode_t fetch_decode_i,
+    output fetch_decode_t fetch_decode_o,
 );
+
+logic [31:0] PCPlus4;
+logic pcCout1, pcCout2;	
+logic [31:0] PCP;
+
+n_bit_register #(32) pc_reg(CLK, RST, PCP, 1'b1, fetch_decode_o.PC);
+
+sumador_nbit #(32) pc_adder1 (fetch_decode_o.PC, 4, 0, pcCout1, PCPlus4);
+
+n_bit_mux #(32, 1) pc_mux ({fetch_decode_i.WD3, PCPlus4}, fetch_decode_i.PCSrc, PCP);
+
+sumador_nbit #(32) pc_adder2 (PCPlus4, 4, 0, pcCout2, fetch_decode_o.R15);
+
+endmodule
--- Proyecto1/Utils/utils.sv
+++ Proyecto1/Utils/utils.sv
@@ -1,5 +1,36 @@
 // Fetch - Decode Pipeline Register Data
 typedef struct packed {
+    logic PCSrc;                        // Branch
 	logic[31:0] Instruction;			// Raw instruction data
 	logic [31:0] PC;					// Program counter
+    logic [31:0] WD3;                   // Write Data
+
 } fetch_decode_t;
+
+// Decode - Execute Pipeline Register Data
+typedef struct packed {
+	//Main pipe
+	logic 				PCSrc; 				//If branch 
+	logic 				RegWrite; 			//Indicates if it's instruction that will store value in register when completed
+	logic 				MemToReg; 			//Indicates if value of alu is address, in other words if its a read from memory the final result
+	logic 				MemWrite; 			//Indicates if it's an instruction with memory writing
+	operation_t 	ExeControl;			//Selection for the execution unit operation
+	logic					Branch; 				//Indicates whether or not instruction is a branch
+	operand_type_t 	ExeSrc; 				//Selects between Forward/Register source or immediate for input B
+	operand_type_t 	ExeSrc2; 			//Selects between Forward/Register source or immediate for input A
+	flags_t 	FlagsCondition; 	//Tells the control unit the condition flags, saved from previous result
+
+	logic [23:0] 		immediate;			//Immediate1 with 14 bits
+	logic [31:0] PC;					//Program counter
+	
+	
+	//Forward and stall
+	logic [31:0]		R1_data;	//Data from first lecture from register file
+	logic [31:0] 		R2_data; 	//Data from second register lecture from register file
+
+	logic [3:0] 		Rd_addr;	//Address of destination register if RegWrite
+	logic [3:0] 		R1_addr;	//Address of first register read
+	logic [3:0]			R2_addr;	//Address of second register read
+
+
+} decode_execute_t;
