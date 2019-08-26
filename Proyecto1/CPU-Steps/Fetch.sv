f840f5079ea3bfa4918a4c1bceb0dae1c01aa10c
JoseRIvera07 <ce.joserivera7@gmail.com>
2019-08-25 23:19:38 -0600

    Fetch PC mux edited

--- Proyecto1/CPU-Steps/Fetch.sv
+++ Proyecto1/CPU-Steps/Fetch.sv
@@ -1,6 +1,6 @@
 module Fetch (
 		input logic CLK, RST, PCSrc,
-		input logic [31:0] WD3,
+		input logic [2:0][17:0] WD3,
 		output logic [31:0] R15, PC, Instr
         );
 
@@ -11,9 +11,9 @@ n_bit_register #(32) pc_reg(CLK, RST, PCP, 1'b1, PC);;   //PC register
 
 assign PCPlus4 = PC+ 4;                                  //PCPLUS4
 
-n_bit_mux #(32, 1) pc_mux ({WD3, PCPlus4}, PCSrc, PCP);  // PC MUX
+n_bit_mux #(32, 1) pc_mux ({WD3[0], PCPlus4}, PCSrc, PCP);  // PC MUX
 
-assign fetch_decode_o.R15 = PCPlus4 + 4;                 // PCPlus8
+assign R15 = PCPlus4 + 4;                 				 // PCPlus8
 
 imem imem(PC, Instr);                                    // Intruction Memory
 
