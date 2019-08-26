d7fb8a9f21b7f7c6448912c20c2a7eadc120dc8b
JoseRIvera07 <ce.joserivera7@gmail.com>
2019-08-25 23:22:43 -0600

    Some Fixes on fetch

--- Proyecto1/CPU-Steps/Fetch.sv
+++ Proyecto1/CPU-Steps/Fetch.sv
@@ -7,14 +7,12 @@ module Fetch (
 logic [31:0] PCPlus4;
 logic [31:0] PCP;
 
-n_bit_register #(32) pc_reg(CLK, RST, PCP, 1'b1, PC);;   //PC register
+n_bit_register #(32) pc_reg(CLK, RST, PCP, 1'b1, PC);;   	//PC register
 
-assign PCPlus4 = PC+ 4;                                  //PCPLUS4
+assign PCPlus4 = PC+ 4;                                  	//PCPLUS4
 
-n_bit_mux #(32, 1) pc_mux ({WD3[0], PCPlus4}, PCSrc, PCP);  // PC MUX
+n_bit_mux #(18, 1) pc_mux ({WD3[0], PCPlus4}, PCSrc, PCP);  // PC MUX
 
-assign R15 = PCPlus4 + 4;                 				 // PCPlus8
-
-imem imem(PC, Instr);                                    // Intruction Memory
+assign R15 = PCPlus4 + 4;                 					// PCPlus8
 
 endmodule
