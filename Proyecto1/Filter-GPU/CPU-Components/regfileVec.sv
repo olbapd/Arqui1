3cb91d5b737386ac77823256f9815cb23f1b9eee
JoseRIvera07 <ce.joserivera7@gmail.com>
2019-08-31 14:31:01 -0600

    Fetch checked

--- Proyecto1/Filter-GPU/CPU-Components/regfileVec.sv
+++ Proyecto1/Filter-GPU/CPU-Components/regfileVec.sv
@@ -3,15 +3,15 @@ module regfileVec (
     input logic clk, 
     input logic we3,
 	input logic [3:0] ra1, ra2, ra3,
-	input logic [2:0][17:0] wd3, r15,
+	input logic [2:0][17:0] wd3,
 	output logic [2:0][17:0] rd1, rd2
     );
 
 // Register Table
-logic [14:0][2:0][17:0] register_table;
+logic [15:0][2:0][17:0] register_table;
 logic [15:0][2:0][17:0] full_register_table;
 
-assign full_register_table = {r15, register_table};
+assign full_register_table = { register_table};
 
 assign rd1 = full_register_table[ra1];
 assign rd2 = full_register_table[ra2];
--- Proyecto1/Filter-GPU/CPU-Steps/Decode.sv
+++ Proyecto1/Filter-GPU/CPU-Steps/Decode.sv
@@ -1,7 +1,7 @@
 module Decode (
     input logic clk, RegWriteW,ImmSrcD,
     input logic [31:0] Instr,
-    input logic [2:0][17:0] wd3, r15,
+    input logic [2:0][17:0] wd3,
     input logic [3:0] wa3w,
     input logic [2:0] RegSrc,
     output logic [2:0][17:0] rd1, rd2,
@@ -14,7 +14,7 @@ n_bit_mux #(4, 1) ra1_mux ({4'b1111, Instr[19:16]}, RegSrc[0], ra1);
 
 n_bit_mux #(4, 1) ra2_mux ({Instr[15:12], Instr[3:0]}, RegSrc[1], ra2);
 
-regfile registerFile(clk, RegWriteW, ra1, ra2, wa3w, wd3, r15, rd1, rd2);
+regfileVec registerFile(clk, RegWriteW, ra1, ra2, wa3w, wd3, r15, rd1, rd2);
 
 extend_module extend(Instr[23:0], ImmSrcD, ExtImm);
 
