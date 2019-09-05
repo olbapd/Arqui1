9c48f03c204971d92d0fbad784d1f05b6400866e
JoseRIvera07 <ce.joserivera7@gmail.com>
2019-09-04 21:18:04 -0600

    Mux2 PC deleted

--- Proyecto1/Filter-GPU/CPU-Steps/Fetch.sv
+++ Proyecto1/Filter-GPU/CPU-Steps/Fetch.sv
@@ -1,7 +1,7 @@
 module Fetch (
 		input logic clk, rst, EN, PCSrcW, BranchTakenE, // BranchTakenE = BranchE && CondEXE  
 		input logic [2:0][17:0] wd3,
-		output logic [17:0] PC
+		output logic [31:0] PC
 );
 
 logic [17:0] PCP, PCNext, PCPlus4;
@@ -10,8 +10,8 @@ n_bit_register #(32) pc_reg(clk, rst, PCNext, EN, PC);   			//PC register
 
 assign PCPlus4 = PC+ 4;                                  			//PCPLUS4
 
-n_bit_mux #(18, 1) pc_mux1 ({wd3[0], PCPlus4}, PCSrcW, PCP); 		// PC MUX1
+n_bit_mux #(18, 1) pc_mux1 ({wd3[0], PCPlus4}, PCSrcW, PCNext); 		// PC MUX1
 
-n_bit_mux #(18, 1) pc_mux2 ({PCP, PCPlus4}, BranchTakenE, PCNext);  // PC MUX2
+//n_bit_mux #(18, 1) pc_mux2 ({PCP, PCPlus4}, BranchTakenE, PCNext);  // PC MUX2
 
 endmodule
--- Proyecto1/Filter-GPU/Control-Unit/main_decoder.sv
+++ Proyecto1/Filter-GPU/Control-Unit/main_decoder.sv
@@ -8,19 +8,19 @@ module main_decoder(Op, Funct_FO, RegW, MemW, MemToReg, ALUSrc, ImmSrc, RegSrc, 
 	output logic [1:0] ImmSrc, RegSrc;
 	
 	// For ease of assigment
-	logic [9:0] logicOutputs;
-	assign {B, MemToReg, MemW, ALUSrc, ImmSrc, RegW, RegSrc, ALUOp} = logicOutputs;
+	logic [8:0] logicOutputs;
+	assign {MemToReg, MemW, ALUSrc, ImmSrc, RegW, RegSrc, ALUOp} = logicOutputs;
 	
 	logic [3:0] caseCond;
 	assign caseCond = {Op, Funct_FO};
 	always @(*) begin
 		logicOutputs <= 0;
 		casex (caseCond)
-		  4'b000x : logicOutputs <= 10'b0000111001;
-		  4'b001x : logicOutputs <= 10'b0001001001;
-		  4'b01x0 : logicOutputs <= 10'b0011010100; //STR
-		  4'b01x1 : logicOutputs <= 10'b0101011000; //LDR
-		  4'b10xx : logicOutputs <= 10'b1001100010;
+		  4'b000x : logicOutputs <= 9'b000111001;
+		  4'b001x : logicOutputs <= 9'b001001001;
+		  4'b01x0 : logicOutputs <= 9'b011010100; //STR
+		  4'b01x1 : logicOutputs <= 9'b101011000; //LDR
+		  4'b10xx : logicOutputs <= 9'b001100010;
 		  default : logicOutputs <= 0;
 		endcase
 	end
