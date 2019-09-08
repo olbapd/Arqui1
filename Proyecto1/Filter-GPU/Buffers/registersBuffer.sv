db620467b1f62dcd3b434645ea4e031410a4be23
marcf98 <32026019+marcf98@users.noreply.github.com>
2019-09-08 11:22:42 -0600

    Update registersBuffer.sv
    deleted branch and flags from buffer

--- Proyecto1/Filter-GPU/Buffers/registersBuffer.sv
+++ Proyecto1/Filter-GPU/Buffers/registersBuffer.sv
@@ -4,13 +4,13 @@ module registersBuffer #(parameter N=18)(
 		input logic [3:0] ra1,ra2,
 		input logic [2:0][N-1:0] extend,
 		input logic clk, reset,load,
-		input logic RegWrite, MemtoReg, MemWrite, Branch, ALUSrc, FlagWrite,
+		input logic RegWrite, MemtoReg, MemWrite, ALUSrc,
 		input logic [3:0] ALUControl, WA3,
 		output logic [2:0][N-1:0] rd1o,
 		output logic [2:0][N-1:0] rd2o,
 		output logic [3:0] ra1o, ra2o,
 		output logic [2:0][N-1:0] extendO,
-		output logic RegWriteO, MemtoRegO, MemWriteO, BranchO, ALUSrcO, FlagWriteO,
+		output logic RegWriteO, MemtoRegO, MemWriteO, ALUSrcO,
 		output logic [3:0] ALUControlO, WA3O
 		);
 	 
@@ -31,9 +31,7 @@ module registersBuffer #(parameter N=18)(
 				RegWriteO = 1'b0;
 				MemtoRegO = 1'b0;
 				MemWriteO = 1'b0;
-				BranchO = 1'b0;
 				ALUSrcO = 1'b0;
-				FlagWriteO = 1'b0;
 				ALUControlO = 4'b0;
 				WA3O = 4'b0;
 				end
@@ -47,9 +45,7 @@ module registersBuffer #(parameter N=18)(
 				RegWriteO = RegWrite;
 				MemtoRegO = MemtoReg;
 				MemWriteO = MemWrite;
-				BranchO = Branch;
 				ALUSrcO = ALUSrc;
-				FlagWriteO = FlagWrite;
 				ALUControlO = ALUControl;
 				WA3O = WA3;
 				end
