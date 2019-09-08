76dc44832412ee79760b7110be14f5135ba7ad38
marcf98 <32026019+marcf98@users.noreply.github.com>
2019-09-07 19:01:01 -0600

    Testbench fetch
    se corrigio la unidad de fetch y se creo testbench

--- Proyecto1/Filter-GPU/CPU-Steps/Fetch.sv
+++ Proyecto1/Filter-GPU/CPU-Steps/Fetch.sv
@@ -6,9 +6,12 @@ module Fetch (
 
 logic [17:0] PCP, PCNext, PCPlus4;
 
+initial 
+PC = 32'b0;
+
 n_bit_register #(32) pc_reg(clk, rst, PCNext, EN, PC);   			//PC register
 
-assign PCPlus4 = PC+ 4;                                  			//PCPLUS4
+assign PCPlus4 = PC+ 18'd4;                                  			//PCPLUS4
 
 n_bit_mux #(18, 1) pc_mux1 ({wd3[0], PCPlus4}, PCSrcW, PCNext); 		// PC MUX1
 
---
+++ Proyecto1/Filter-GPU/TestBenchs/DecodeTB.sv
@@ -0,0 +1,15 @@
+module DecodeTB ();
+
+	logic clk, RegWriteW,ImmSrcD;
+    logic [31:0] Instr;
+    logic [2:0][17:0] wd3;
+    logic [3:0] wa3w;
+    logic [2:0] RegSrc;
+    logic [2:0][17:0] rd1, rd2;
+    logic [2:0][17:0] ExtImm;
+
+	initial begin
+
+	end
+
+endmodule
