08ffce74823bc954a8abaab730111c86ed8196c3
olbapd <pablod.garciab@gmail.com>
2019-11-11 17:25:09 -0600

    Add UART.

--- Examen/MP/Examen.X/main.c
+++ Examen/MP/Examen.X/main.c
@@ -8,13 +8,14 @@
 #include <lcd.c>
 #include <kbd.c>
 #include <stdio.h>
+#use rs232(uart1, baud = 9600)                // Initialize UART module
 
 float temperatura;
 char keypad;
 int i;
 char keys_pressed[5];
 
-void checkTemp(){
+void checkTemp(int value){
     if(temperatura<20)  {
         //Abre
         output_low(PIN_D3);
@@ -66,9 +67,19 @@ void checkTemp(){
     lcd_gotoxy(12,1);
     printf (lcd_putc,"C");
     
+    putc(13);                                   // Go to first column
+    putc(10);                                   // Start a new lin
+    if(value==1){
+        printf("Code:12345-Temp:%fC \n",temperatura );
+    }
+    else if(value==2){
+        printf("Code:67890-Temp:%fC \n",temperatura );
+    }
+    
 }
 
 void main() {
+    
     int checkPassword=0;
     int valid=1;
     int maintinaing=0;
@@ -80,25 +91,27 @@ void main() {
     keys_pressed[4]='*';
     float previousTemp=0;
     
-    
-    
     setup_adc_ports(ALL_ANALOG); 
     setup_adc(ADC_CLOCK_INTERNAL);
-    //set_adc_channel(0);
     setup_ccp1(0x0C);
     setup_ccp2(0x0C);
-    setup_timer_2(T2_DIV_BY_16,255,1);
+    //setup_timer_2(T2_DIV_BY_16,255,2); //For 8MHz
+    setup_timer_2(T2_DIV_BY_16,255,1); //For 4MHz
     output_high(PIN_D3);
 
     kbd_init();
-    lcd_init();                                  
-    port_b_pullups(TRUE);   //habilitan las resistencias pull up para evitar resistencias en el teclado
+    lcd_init();
 
+    port_b_pullups(TRUE);   //habilitan las resistencias pull up para evitar resistencias en el teclado
     i=0;
+    delay_ms(2000);
+    printf("Examen Pablo Garcia Brenes y Marco Fernandez Granados.\n");
+    
     
     while (TRUE)   {
         //Muestra Temperatura
         temperatura=(float)read_adc()/2;
+        temperatura=temperatura-1;
         buttonState=input_state(PIN_C5);
         if(buttonState==0){
             if(maintinaing==0){
@@ -150,7 +163,7 @@ void main() {
                     i=0;
                     printf(lcd_putc,"\fPIN Correcto");
                     delay_ms(2000);
-                    checkTemp();
+                    checkTemp(1);
                 }
                 else if(keys_pressed[0]=='6' && 
                         keys_pressed[1]=='7' && 
@@ -160,7 +173,7 @@ void main() {
                     i=0;
                     printf(lcd_putc,"\fPIN Correcto");
                     delay_ms(2000);
-                    checkTemp();
+                    checkTemp(2);
                 }
                 else {
                     printf(lcd_putc,"\fPIN Incorrecto");
